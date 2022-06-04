cde39f2aee7ac2f9986da343258c1f4965e019e5
Fikremariam Deyas <fikredeyas1@gmail.com>
2022-06-04 17:15:10 -0400

    commit tree 835c49996f3dec6d37d2db8ab573878a37c41828

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(list, word) {
+    let cards = []
+    for (let i = 0; i < list.length; i++) {
+        cards.push("Thank you, " + list[i] + ", for the wonderful " + word + " gift!")
+    }
+    return cards
+}
+
+function countDown(number){
+    for (let i = number; i >= 0; i--) { 
+        console.log(i)
+     }
+}
